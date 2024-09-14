d6b3ae4a478a013e34eadc6bc02ffd3e86582a3f
Aimmzz <rohimkurniawan456@gmail.com>
2024-09-14 09:35:16 +0700

    feat: cretae architecture

---
+++ lib/features/article/detail_article_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailArticlePage extends StatefulWidget {
+  const DetailArticlePage({super.key});
+
+  @override
+  State<DetailArticlePage> createState() => _DetailArticlePageState();
+}
+
+class _DetailArticlePageState extends State<DetailArticlePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
